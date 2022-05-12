95718fda64f9413facfc1af8cc408b00f7ade29f
Jeremy Reed <reeje76@gmail.com>
2022-05-12 19:35:00 -0400

    Some basic refactors.
    Make start time definitions for each phase / transition const.
    Make color temperature const as well.
    Both of these changes make it easier to access these constants in code,
    as well as tests.
    Also add some tests.  Currently just testing the phases.
    The code is not yet interpolating the transition color temperatures yet.
    The code assumes a fixed color temperature for the transitions.
    A future commit will fix this.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use std::process::Command;
 use chrono::prelude::*;
 
+const DAY_START: (u32, u32, u32) = (7, 0, 0);
+
+// Color temperature of the static phases.
+const DAY_TEMP: i64 = 6500;
+const EVENING_TEMP: i64 = 3000;
+const NIGHT_TEMP: i64 = 1900;
+
+// Temporary.. For now.
+const NIGHT_TO_DAY_TEMP: i64 = 2200;
+const DAY_TO_EVENING_TEMP: i64 = 5000;
+const EVENING_TO_NIGHT_TEMP: i64 = 2200;
+
 fn execute_redshift(temp: &i64) {
     let temp_str = temp.to_string();
 
@@ -16,20 +28,87 @@ fn interpolate_color_temperature(start_time: i64, end_time: i64, current_time: i
     unimplemented!();
 }
 
-fn main() {
-    let local_now: DateTime<Local> = Local::now();
+fn get_color_temperature(current_time: DateTime<Local>) -> i64 {
+    // Start times.
+    let night_to_day_start = Local.ymd(current_time.year(), current_time.month(), current_time.day()).and_hms(6, 0, 0);
+    let day_start = Local.ymd(current_time.year(), current_time.month(), current_time.day()).and_hms(DAY_START.0, DAY_START.1, DAY_START.2);
+    let day_to_evening_start = Local.ymd(current_time.year(), current_time.month(), current_time.day()).and_hms(17, 0, 0);
+    let evening_start =Local.ymd(current_time.year(), current_time.month(), current_time.day()).and_hms(18, 0, 0); 
+    let evening_to_night_start = Local.ymd(current_time.year(), current_time.month(), current_time.day()).and_hms(20, 0, 0);
+    let night_start = Local.ymd(current_time.year(), current_time.month(), current_time.day()).and_hms(22, 0, 0);
 
-    // Start time of these phases.
-    let local_morning = Local.ymd(local_now.year(), local_now.month(), local_now.day()).and_hms(6, 0, 0);
-    let local_night = Local.ymd(local_now.year(), local_now.month(), local_now.day()).and_hms(20, 0, 0);
-
-    // Color temperature of the static phases.
-    let night_temp = 1900;
-    let day_temp = 6500;
-
-    if local_now >= local_morning && local_now < local_night {
-        execute_redshift(&day_temp);
+    // Phase checks.
+    if current_time >= night_start || current_time <= night_to_day_start {
+        // night.
+        println!("night");
+        return NIGHT_TEMP;
+    } else if current_time > night_to_day_start && current_time < day_start {
+        // night_to_day.
+        println!("night_to_day");
+        return NIGHT_TO_DAY_TEMP;
+    } else if current_time >= day_start && current_time <= day_to_evening_start {
+        // day.
+        println!("day");
+        return DAY_TEMP;
+    } else if current_time > day_to_evening_start && current_time < evening_start {
+        // day_to_evening.
+        println!("day_to_evening");
+        return DAY_TO_EVENING_TEMP;
+    } else if current_time >= evening_start && current_time <= evening_to_night_start {
+        // evening.
+        println!("evening");
+        return EVENING_TEMP;
+    } else if current_time > evening_to_night_start && current_time < night_start {
+        // evening_to_night.
+        println!("evening_to_night");
+        return EVENING_TO_NIGHT_TEMP;
     } else {
-        execute_redshift(&night_temp);
+        // Shouldn't ever get here.
+        println!("invalid!");
+        panic!("Invalid time, did not match a phase/transition as expected!");
+    }
+}
+
+fn main() {
+    let current_time: DateTime<Local> = Local::now();
+
+    let temperature: i64 = get_color_temperature(current_time);
+
+    execute_redshift(&temperature);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod get_color_temperature_tests {
+        use super::*;
+
+        #[test]
+        fn should_return_night_temp_at_late_night() {
+            let time = Local.ymd(2022, 5, 20).and_hms(23, 0, 0);
+
+            let actual = get_color_temperature(time);
+
+            assert_eq!(NIGHT_TEMP, actual);
+        }
+
+        #[test]
+        fn should_return_night_temp_at_early_morning() {
+            let time = Local.ymd(2022, 5, 20).and_hms(2, 0, 0);
+
+            let actual = get_color_temperature(time);
+
+            assert_eq!(NIGHT_TEMP, actual);
+        }
+
+        #[test]
+        fn should_return_night_to_day_temp_() {
+            let time = Local.ymd(2022, 5, 20).and_hms(6, 30, 0);
+
+            let actual = get_color_temperature(time);
+
+            assert_eq!(NIGHT_TO_DAY_TEMP, actual);
+        }
     }
 }
